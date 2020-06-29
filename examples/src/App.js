6e89a1d9ddecad8f0f693b9b74572df874f098b8
David Lindkvist <david@14islands.com>
2020-06-29 22:33:33 +0200

    test to add examples app

---
+++ examples/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { GlobalCanvas } from 'r3f-scroll-rig'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
