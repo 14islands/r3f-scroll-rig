4c515340ddb971594c30af6c0dc61ab6c5df9d51
David Lindkvist <david@14islands.com>
2020-10-12 22:41:13 +0200

    build: external deps

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
 const external = (id) => !id.startsWith('.') && !id.startsWith(root)
@@ -43,6 +44,7 @@ function createConfig(entry, out) {
       output: { file: `dist/${out}.js`, format: 'esm' },
       external,
       plugins: [
+        peerDepsExternal(),
         json(),
         babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
         sizeSnapshot(),
@@ -56,6 +58,7 @@ function createConfig(entry, out) {
       output: { file: `dist/${out}.cjs.js`, format: 'cjs' },
       external,
       plugins: [
+        peerDepsExternal(),
         json(),
         babel(getBabelOptions({ useESModules: false })),
         sizeSnapshot(),
