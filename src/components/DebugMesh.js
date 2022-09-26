5b70ac5c7323ca27d2c383a5e0348a89359c7530
David Lindkvist <david@14islands.com>
2022-09-26 22:34:49 +0200

    fix: remove drei dep

--- src/components/DebugMesh.js
+++ src/components/DebugMesh.js
@@ -1,33 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { extend } from '@react-three/fiber'
-import { shaderMaterial } from '@react-three/drei/core/shaderMaterial.js'
 import { Color } from 'three'
-
-const DebugMaterial = shaderMaterial(
-  { color: new Color(1.0, 0.0, 0.0), opacity: 1 },
-  // vertex shader
-  ` varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }`,
-  // fragment shader
-  `
-    uniform vec3 color;
-    uniform float opacity;
-    varying vec2 vUv;
-    void main() {
-      gl_FragColor.rgba = vec4(color, opacity);
-    }
-  `
-)
-extend({ DebugMaterial })
 
 export const DebugMesh = ({ scale }) => (
   <mesh scale={scale}>
     <planeBufferGeometry />
-    <debugMaterial color="hotpink" transparent opacity={0.5} />
+    <shaderMaterial
+      args={[
+        {
+          uniforms: {
+            color: { value: new Color('hotpink') },
+          },
+          vertexShader: `
+            void main() {
+              gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
+          `,
+          fragmentShader: `
+            uniform vec3 color;
+            uniform float opacity;
+            void main() {
+              gl_FragColor.rgba = vec4(color, .5);
+            }
+          `,
+        },
+      ]}
+      transparent
+    />
   </mesh>
 )
 
