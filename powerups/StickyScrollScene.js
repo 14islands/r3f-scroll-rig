f30864ffc94ac3ce3c39ae5857817cedef312c8f
David Lindkvist <david@14islands.com>
2022-09-26 22:40:05 +0200

    fix: fixed sticky scroll scene after refactor

--- powerups/StickyScrollScene.js
+++ powerups/StickyScrollScene.js
@@ -1,70 +1,75 @@
-import { useRef } from 'react'
-import { MathUtils } from 'three'
-import { useFrame } from '@react-three/fiber'
+import { useRef, useMemo } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
 import { ScrollScene } from '@14islands/r3f-scroll-rig'
+import lerp from '@14islands/lerp'
 
 // Sticky mesh that covers full viewport size
-export const StickyMesh = ({ children, scrollState, lerp, scale, priority, stickyLerp = 1.0 }) => {
-  const mesh = useRef()
-  const local = useRef({ lerp: 1 }).current
+export const StickyChild = ({ children, scrollState, scale, priority, stickyLerp = 1.0 }) => {
+  const group = useRef()
+  const size = useThree((s) => s.size)
 
-  useFrame(() => {
+  useFrame((_, delta) => {
     if (!scrollState.inViewport) return
 
     //  move to top of sticky area
-    const yTop = scale[1] / 2 - scale.viewportHeight * 0.5
-    const yBottom = -scale[1] / 2 + scale.viewportHeight * 0.5
-    const ySticky = yTop - (scrollState.viewport - 1) * scale.viewportHeight
+    const yTop = scale[1] * 0.5 - size.height * 0.5
+    const yBottom = -scale[1] * 0.5 + size.height * 0.5
+    const ySticky = yTop - (scrollState.viewport - 1) * size.height
 
-    let y = mesh.current.position.y
-    let targetLerp
+    let y = group.current.position.y
 
     // enter
     if (scrollState.viewport < 1) {
       y = yTop
-      targetLerp = 1
     }
     // sticky
-    else if (scrollState.viewport > 1 && scrollState.visibility < 1) {
+    else if (scrollState.visibility < 1) {
       y = ySticky
-      targetLerp = stickyLerp
     }
     // exit
     else {
       y = yBottom
-      // TODO figure out soft limits
-      // const f = Math.max(1, scrollState.visibility - 1)
-      // y =  MathUtils.lerp(ySticky, yBottom, f)
-      targetLerp = 1
     }
 
-    local.lerp = MathUtils.lerp(local.lerp, targetLerp, stickyLerp < 1 ? lerp : 1)
-    mesh.current.position.y = MathUtils.lerp(mesh.current.position.y, y, local.lerp)
-  }, priority + 1) // must happen after ScrollScene's useFrame to be buttery
+    group.current.position.y = lerp(group.current.position.y, y, stickyLerp, delta)
+  }, priority) // must happen after ScrollScene's useFrame to be buttery
 
-  return <mesh ref={mesh}>{children}</mesh>
+  return <group ref={group}>{children}</group>
 }
 
-export const renderAsSticky = (children, { stickyLerp, scaleToViewport }) => {
+export const renderAsSticky = (children, size, { stickyLerp, scaleToViewport }) => {
   return ({ scale, ...props }) => {
     // set child's scale to 100vh/100vw instead of the full DOM el
     // the DOM el should be taller to indicate how far the scene stays sticky
     let childScale = scale
     if (scaleToViewport) {
-      childScale = { ...scale, width: scale.viewportWidth, height: scale.viewportHeight }
+      childScale = [size.width, size.height, 1]
     }
     return (
-      <StickyMesh scale={scale} stickyLerp={stickyLerp} {...props}>
+      <StickyChild scale={scale} stickyLerp={stickyLerp} {...props}>
         {children({ scale: childScale, ...props })}
-      </StickyMesh>
+      </StickyChild>
     )
   }
 }
 
-export const StickyScrollScene = ({ children, stickyLerp, scaleToViewport = true, ...props }) => {
+export const StickyScrollScene = ({ children, track, stickyLerp, scaleToViewport = true, ...props }) => {
+  const size = useThree((s) => s.size)
+
+  const internalRef = useRef(track.current)
+
+  // if tracked element is position:sticky, track the parent instead
+  // we want to track the progress of the entire sticky area
+  useMemo(() => {
+    const style = getComputedStyle(track.current)
+    if (style.position === 'sticky') {
+      internalRef.current = track.current.parentElement
+    }
+  }, [track])
+
   return (
-    <ScrollScene scissor={false} {...props}>
-      {renderAsSticky(children, { stickyLerp, scaleToViewport })}
+    <ScrollScene track={internalRef} {...props}>
+      {renderAsSticky(children, size, { stickyLerp, scaleToViewport })}
     </ScrollScene>
   )
 }
