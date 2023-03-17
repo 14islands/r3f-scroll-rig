8418e8535860ac2067a4656ea791c6ff7db1f388
David Lindkvist <david@14islands.com>
2023-03-17 11:00:23 +0100

    fix: clear canvas properly when empty

--- src/components/GlobalChildren.tsx
+++ src/components/GlobalChildren.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, ReactNode, cloneElement } from 'react'
-import { invalidate } from '@react-three/fiber'
+import { invalidate, useThree } from '@react-three/fiber'
 
 import { useCanvasStore } from '../store'
 import { useScrollRig } from '../hooks/useScrollRig'
@@ -8,6 +8,7 @@ import { useScrollRig } from '../hooks/useScrollRig'
  * Renders global children from useCanvas hook
  */
 export const GlobalChildren = ({ children }: { children?: ReactNode }) => {
+  const gl = useThree((s) => s.gl)
   const canvasChildren = useCanvasStore((state) => state.canvasChildren)
   const scrollRig = useScrollRig()
 
@@ -15,6 +16,9 @@ export const GlobalChildren = ({ children }: { children?: ReactNode }) => {
     // render empty canvas automatically if all children were removed
     if (!Object.keys(canvasChildren).length) {
       scrollRig.debug && console.log('GlobalRenderer', 'auto render empty canvas')
+      // clear leftover viewports etc from unmounted components
+      gl.clear()
+      // re-render global scene in case frameloop="demand" to avoid empty canvas
       scrollRig.requestRender()
       invalidate()
     }
