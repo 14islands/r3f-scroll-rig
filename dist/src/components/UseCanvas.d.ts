6ce8c73f61830e37a211704fd5dd04d8946dc9af
David Lindkvist <david@14islands.com>
2023-05-26 13:46:11 +0200

    new build

--- dist/src/components/UseCanvas.d.ts
+++ dist/src/components/UseCanvas.d.ts
@@ -4,6 +4,7 @@ interface IUseCanvas {
     children: ReactNode | ((props: ScrollRigState) => ReactNode);
     id?: string;
     dispose?: boolean;
+    [key: string]: any;
 }
-declare const UseCanvas: import("react").ForwardRefExoticComponent<IUseCanvas & import("react").RefAttributes<unknown>>;
+declare const UseCanvas: import("react").ForwardRefExoticComponent<Pick<IUseCanvas, keyof IUseCanvas> & import("react").RefAttributes<unknown>>;
 export { UseCanvas };
