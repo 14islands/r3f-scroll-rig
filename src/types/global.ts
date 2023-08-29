4287bc2224cd4482ca0ee7b13848b24a2d578581
David Lindkvist <david@14islands.com>
2023-08-29 09:08:36 +0200

    fix: improve types for vecn

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,9 +1,27 @@
+declare type vec2 = {
+  x: number
+  y: number
+  xy: vec2
+  yx: vec2
+  times: (n: number) => vec2
+  div: (n: number) => vec2
+  max: () => number
+  min: () => number
+  sum: () => number
+} & [x: number, y: number]
+
 declare type vec3 = {
   x: number
   y: number
   z: number
-  xy: [x: number, y: number]
-  xyz: [x: number, y: number, z: number]
+  xy: vec2
+  yx: vec2
+  xyz: vec3
+  xzy: vec3
+  yxz: vec3
+  yzx: vec3
+  zxy: vec3
+  zyx: vec3
   times: (n: number) => vec3
   div: (n: number) => vec3
   max: () => number
@@ -12,5 +30,6 @@ declare type vec3 = {
 } & [x: number, y: number, z: number]
 
 declare module 'vecn' {
+  export function vec2(x: number, y: number): vec2
   export function vec3(x: number, y: number, z: number): vec3
 }
