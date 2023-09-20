61ee748d576a28c304ea80751c38dd76f3dd879d
David Lindkvist <david@14islands.com>
2023-09-20 11:54:07 +0200

    fix: use vanilla raf for scrollbar if webgl error

--- src/components/R3FSmoothScrollbar.tsx
+++ src/components/R3FSmoothScrollbar.tsx
@@ -2,8 +2,11 @@ import React, { forwardRef } from 'react'
 import { SmoothScrollbar } from '../scrollbar/SmoothScrollbar'
 import { ISmoothScrollbar } from '../scrollbar/SmoothScrollbarTypes'
 import { addEffect, invalidate } from '@react-three/fiber'
+import { useCanvasStore } from '../store'
 
 function R3FSmoothScrollbar(props: ISmoothScrollbar, ref: any) {
+  const isCanvasAvailable = useCanvasStore((s) => s.isCanvasAvailable)
+  if (!isCanvasAvailable) return <SmoothScrollbar ref={ref} {...props} />
   return <SmoothScrollbar ref={ref} invalidate={invalidate} addEffect={addEffect} {...props} />
 }
 
